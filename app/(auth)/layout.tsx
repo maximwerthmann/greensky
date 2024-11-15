76d953b9942bba8caac547c4ba3c95d819afd22d
maximwerthmann <maxim-werthmann@outlook.com>
2024-11-16 00:00:38 +0100

    Pre-Alpha 0.0.1.1

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main className="flex-1 flex items-center justify-center bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+    <main className="flex-1 flex items-center justify-center bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
       <div className="flex flex-col space-y-8 items-center">
       {children}
       </div>
--- app/(auth)/providers.tsx
+++ app/(auth)/providers.tsx
@@ -1,10 +1,21 @@
 import { Button } from "@/components/ui/button";
+import { BsGoogle, BsMicrosoft, BsDiscord, BsTwitterX } from "react-icons/bs"
 
 export default function Providers() {
     return (
-        <div className="flex flex-col space-y-2">
-            <Button variant={'secondary'} className="w-full">Google</Button>
-            <Button variant={'secondary'} className="w-full">Microsoft</Button>
+        <div className="flex items-center justify-evenly">
+            <Button size={'icon'} variant={'secondary'} className="p-6">
+                <BsGoogle/>
+            </Button>
+            <Button size={'icon'} variant={'secondary'} className="p-6">
+                <BsMicrosoft/>
+            </Button>
+            <Button size={'icon'} variant={'secondary'} className="p-6">
+                <BsDiscord/>
+            </Button>
+            <Button size={'icon'} variant={'secondary'} className="p-6">
+                <BsTwitterX/>
+            </Button>
         </div>
     )
 }
